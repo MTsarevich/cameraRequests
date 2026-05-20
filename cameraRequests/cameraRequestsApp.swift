8cd325eafbf8e059a1f0365ba00325bfdbcd8ad5
MTsarevich <maksim.tsarevich@gmail.com>
2026-05-20 23:25:41 +0300

    Initial Commit

---
+++ cameraRequests/cameraRequestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cameraRequestsApp.swift
+//  cameraRequests
+//
+//  Created by Maksim Tsarevich on 20.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct cameraRequestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
